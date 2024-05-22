f4c3aca61a4d28a8f374e23ea19b7140073290dc
nicoleyimessier <nicoleyimessier@gmail.com>
2024-05-22 13:05:36 -0400

    updated prompt

--- src/claude_instructor_test/__init__.py
+++ src/claude_instructor_test/__init__.py
@@ -29,9 +29,9 @@ class Response(BaseModel):
 client = instructor.from_cohere(cohere.Client())
 
 task = f"""\
-Given the following sentence, create a Response object that contains an Emotion object with an associated list of 5 Color objects to describe the emotion.
+Given the following sentence, create a Response object that contains an Emotion object with an associated list of 2 Color objects to describe the emotion.
 
-The Emotion name should be a real emotion, and only one word.
+The Emotion name should be a real emotion, and only one word. The RGB values will be used to animate LEDs so there is no black or gray light.
 
 Sentence: {args.secret}
 """
